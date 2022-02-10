55ad1be6ffc4cd442a376f0fbb38fc842882f36d
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2022-02-10 15:51:39 +0000

    Handle canvas controls ops in mouse handler

--- site/pages/frankenscape/js/mouse.js
+++ site/pages/frankenscape/js/mouse.js
@@ -1,25 +1,37 @@
 mk.CanvasGame.prototype.initMouse = function() {
 
-  console.log('init mouse');
+  // MOUSE MOVE
 
-  // TODO now that we're using a "new Path2D()" when the shape moves, our usage of the
-  // "const" shapes below no longer works!
+  canvas.addEventListener('mousemove', function(e) {
 
-  // mousemove
-  canvas.addEventListener('mousemove', function(event) {
+    e.preventDefault();
 
-    event.preventDefault();
+//    console.log(e.offsetX, e.offsetY);
 
-//    console.log(event.offsetX, event.offsetY);
-
-    var x = event.offsetX;
-    var y = event.offsetY;
+    var x = e.offsetX;
+    var y = e.offsetY;
 
     // Hang onto the last mouse coordinates.
     game.setMouseX(x);
     game.setMouseY(y);
 
-    if (game.isPaused()) { return; }
+    if (game.isPaused()) {
+
+      switch (CANVAS_CTRL_OP) {
+
+        case 'drawCircle':
+          drawCircleMouseMove(x, y, e);
+          break;
+
+        case 'drawRectangle':
+          drawRectangleMouseMove(x, y, e);
+          break;
+
+      }
+
+    }
+
+    // The game is not paused...
 
     // Rock angle and velocity.
     var deltaX = Math.abs(x - myFrankenstein.x);
@@ -36,29 +48,43 @@ mk.CanvasGame.prototype.initMouse = function() {
     angleBadge.innerHTML = parseFloat(launchAngle).toFixed(3);
     velocityBadge.innerHTML = parseFloat(launchVelocity).toFixed(1);
 
+    // TODO now that we're using a "new Path2D()" when the shape moves, our usage of the
+  // "const" shapes below no longer works!
+
     // circle - "mouse over"
-//    myBall.ctx.fillStyle = context.isPointInPath(circle, event.offsetX, event.offsetY) ?
+//    myBall.ctx.fillStyle = context.isPointInPath(circle, e.offsetX, e.offsetY) ?
 //      'red' : 'purple';
 
     // square - "mouse over"
-//    mySquare.ctx.strokeStyle = context.isPointInPath(square, event.offsetX, event.offsetY) ?
+//    mySquare.ctx.strokeStyle = context.isPointInPath(square, e.offsetX, e.offsetY) ?
 //      'green' : 'blue';
 
   });
 
-  // mousedown
-  canvas.addEventListener('mousedown', function(event) {
+  // MOUSE DOWN
 
-    event.preventDefault();
+  canvas.addEventListener('mousedown', function(e) {
 
-    console.log('click', event.offsetX, event.offsetY);
+    e.preventDefault();
+    var x = e.offsetX;
+    var y = e.offsetY;
+
+    console.log('click', x, y);
 
     if (game.isPaused()) {
 
       switch (CANVAS_CTRL_OP) {
 
+        case 'drawCircle':
+          drawCircleMouseDown(x, y, e);
+          break;
+
+        case 'drawRectangle':
+          drawRectangleMouseDown(x, y, e);
+          break;
+
         case 'drawPolygon':
-          console.log('draw a polygon dude');
+          drawPolygonMouseDown(x, y, e); // TODO why are we calling a "click" handler here?
           break;
 
       }
@@ -81,12 +107,65 @@ mk.CanvasGame.prototype.initMouse = function() {
     }
 
     // Check whether click is inside circle
-//    if (context.isPointInPath(circle, event.offsetX, event.offsetY)) {
+//    if (context.isPointInPath(circle, e.offsetX, e.offsetY)) {
 //      console.log('inside circle');
 //    }
 
     // Check whether click is inside square
-//    if (context.isPointInPath(square, event.offsetX, event.offsetY)) {
+//    if (context.isPointInPath(square, e.offsetX, e.offsetY)) {
+//      console.log('inside square');
+//    }
+
+  });
+
+  // MOUSE UP
+
+  canvas.addEventListener('mouseup', function(e) {
+
+    e.preventDefault();
+    var x = e.offsetX;
+    var y = e.offsetY;
+
+    console.log('release', x, y);
+
+    if (game.isPaused()) {
+
+      switch (CANVAS_CTRL_OP) {
+
+        case 'drawCircle':
+          drawCircleMouseUp(x, y, e);
+          break;
+
+        case 'drawRectangle':
+          drawRectangleMouseUp(x, y, e);
+          break;
+
+      }
+
+      return;
+
+    }
+
+    if (myRock.inHand) {
+
+      // TODO shouldn't this start the throw behavior's animationTimer?
+
+      myRock.velocityX = myRock.launchVelocity * Math.cos(myRock.launchAngle);
+      myRock.velocityY = myRock.launchVelocity * Math.sin(myRock.launchAngle);
+      myRock.inHand = false;
+      myRock.inFlight = true;
+      myRock.launchTime = +new Date();
+      console.log('THROW!', myRock.velocityX, myRock.velocityY);
+
+    }
+
+    // Check whether click is inside circle
+//    if (context.isPointInPath(circle, e.offsetX, e.offsetY)) {
+//      console.log('inside circle');
+//    }
+
+    // Check whether click is inside square
+//    if (context.isPointInPath(square, e.offsetX, e.offsetY)) {
 //      console.log('inside square');
 //    }
 
